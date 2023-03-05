82585456b6745c9c480ca1d11b62b47c9618d3d0
Liza <kuznecovaliza11@yandex.ru>
2023-03-06 00:09:35 +0700

    Task 47

---
+++ Task47/Program.cs
@@ -0,0 +1,50 @@
+﻿int GetIncomingData(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+float[,] Create2DArray(int col, int row)
+{
+    return new float[row, col];
+}
+
+void Fill2DArray(float[,] array2D, int low, int high)
+{
+    for (int i = 0; i < array2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength(1); j++)
+        {
+            array2D[i, j] = (new Random().NextSingle() * (high - low) + low);
+        }
+    }
+}
+
+void Print2DArray(float[,] array2D)
+{
+    int LastCol = array2D.GetLength(1);
+    for (int i = 0; i < array2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < LastCol; j++)
+        {
+            if (j == LastCol - 1)
+            {
+                Console.Write(string.Format("{0:F1}", array2D[i, j]));
+            }
+            else
+            {
+                Console.Write($"{string.Format("{0:F1}", array2D[i, j])}; ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+int col = GetIncomingData("Введите количество столбцов");
+int row = GetIncomingData("Введите количество строк");
+float[,] arr2D = Create2DArray(col, row);
+int low = GetIncomingData("Введите нижнюю границу значений элементов массива");
+int high = GetIncomingData("Введите верхнюю границу значений элементов массива");
+Fill2DArray(arr2D, low, high);
+Print2DArray(arr2D);
+
